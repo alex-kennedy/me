cba808013817eec8dd3e87a21519b1349d7eab9a
Alex Kennedy <apkennedy@me.com>
2019-01-10 17:01:08 +0100

    First draft written

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: "Alex Kennedy",
+    author: "Alex Kennedy",
+    description: "My personal site. "
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'alex-kennedy',
+        short_name: 'alex',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/logo.svg', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    // 'gatsby-plugin-offline'
+  ],
+}
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Footer from './Footer'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header id="header">
+                <div className="inner">
+                    <h1><strong>I am Alex Kennedy</strong> <br />
+                    Engineering and Physics student at the University of Auckland, New Zealand. </h1>
+                </div>
+                <Footer />
+            </header>
+        )
+    }
+}
+
+export default Header
